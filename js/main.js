bcc0c3fc642bbf7fce59338d8b389cb6e91ea486
Michal Stepien <michal.stepien36@gmail.com>
2019-03-16 22:48:52 +0100

    rwd menu

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
+const menuFooter = document.querySelector("footer");
+const menuSocial = document.querySelector(".menu-social");
 const navItems = document.querySelectorAll(".nav-item");
 
 // Set Initial State Of Menu
@@ -16,6 +18,8 @@ function toggleMenu() {
     menu.classList.add("show");
     menuNav.classList.add("show");
     menuBranding.classList.add("show");
+    menuFooter.classList.add("show");
+    menuSocial.classList.add("show");
     navItems.forEach(item => item.classList.add("show"));
 
     // Set Menu State
@@ -25,6 +29,8 @@ function toggleMenu() {
     menu.classList.remove("show");
     menuNav.classList.remove("show");
     menuBranding.classList.remove("show");
+    menuFooter.classList.remove("show");
+    menuSocial.classList.remove("show");
     navItems.forEach(item => item.classList.remove("show"));
 
     // Set Menu State
